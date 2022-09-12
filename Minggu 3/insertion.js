ecc208d43e2fa8366354f6593356b1456fea8058
unknown <richardowen2411@gmail.com>
2022-09-12 17:30:32 +0800

    minggu 3

---
+++ Minggu 3/insertion.js
@@ -0,0 +1,30 @@
+const arraySample = "0,-1,1,-2,2,-3,3,-4,4,-5";
+
+function processData(input) {
+    let arraySort;
+    
+    for (data of input.split(",")) {
+        arraySort.push(parseInt(data));
+    }
+
+    // let arraySort = input.split(",");
+
+    // for (let i = 0; i < arraySort.length; i++) {
+    //     arraySort[i] = parseInt(arraySort[i]);
+    // } 
+
+    for (let i = 1; i < arraySort.length; i++) {
+        let temp = arraySort[i];
+        let j = i-1; 
+        while ((j > -1) && (temp < arraySort[j])) {
+            arraySort[j+1] = arraySort[j];
+            j--;
+        }
+        arraySort[j+1] = temp;
+    }
+    return arraySort.join();
+}
+
+const hasil = processData(arraySample);
+
+console.log(hasil);
