278a1dfd4a253f1d399d5f14e7f85c8a5244554b
unknown <richardowen2411@gmail.com>
2022-10-20 14:22:10 +0800

    OOP SORTING

--- Minggu 3/insertion.js
+++ Minggu 3/insertion.js
@@ -1,33 +1,51 @@
-class Insertion{
-    processData(input) {
-        let arraySort = input.split(",");
-        
-        for (let i = 0; i < arraySort.length; i++) {
-            arraySort[i] = parseInt(arraySort[i]);
-        } 
-    
-        for (let i = 1; i < arraySort.length; i++) {
-            console.log("Before Sorting = " + arraySort.join());
-            let temp = arraySort[i];
+class InsertionSort{
+    constructor(input) {
+        this.data = input;
+    }
+
+    insertionSort(arr) {
+        for (let i = 1; i < arr.length; i++) {
+            console.log("Before Sorting = " + arr.join());
+            let temp = arr[i];
             let j = i-1; 
-            while ((j > -1) && (temp < arraySort[j])) {
-                
-                arraySort[j+1] = arraySort[j];
+
+            while ((j > -1) && (temp < arr[j])) {
+                arr[j+1] = arr[j];
                 j--;
             }
-            arraySort[j+1] = temp;
-            console.log("temp = " + temp)
-            console.log("After Sorting = " + arraySort.join() + "\n");
+            
+            arr[j+1] = temp;
+            
+            this.showProcess(temp, arr);
         }
-        return arraySort.join();
+        return arr;
+    }
+
+    processData() {    
+        let arr = this.data.split(",");
+        
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = parseInt(arr[i]);
+        }
+        
+        this.data = this.insertionSort(arr).join();
+    }
+
+    showProcess(temp, arr) {
+        console.log("temp = " + temp)
+        console.log("After Sorting = " + arr.join() + "\n");
+    }
+
+    do() {
+        console.log("Samples = " + this.data + "\n");
+
+        this.processData();
+
+        console.log("Result = " + this.data)
     }
 }
 
+const sample = "-5,5,-4,4,-3,3,-2,2,-1,1,0";
+const insertionSort =  new InsertionSort(sample);
 
-
-const insertion = new Insertion();
-const sample = "-10,5,-8,4,-7,3,20,2,23,60,20,-1";
-
-const hasil = insertion.processData(sample);
-
-console.log("Final = " + hasil);
+insertionSort.do();
--- Minggu 4/QuickSort.js
+++ Minggu 4/QuickSort.js
@@ -1,4 +1,7 @@
 class QuickSort{
+    constructor(input) {
+        this.data = input;
+    }
 
     quickSort(arr) {
         if (arr.length < 2) {
@@ -18,34 +21,45 @@ class QuickSort{
         arr = [pivot].concat(arr);
         for (let i = 1; i < arr.length; i++) {
             if (pivot > arr[i]) {
-            left.push(arr[i]);
+                left.push(arr[i]);
             } else {
-            right.push(arr[i]);
+                right.push(arr[i]);
             }
         }
-        console.log("Pivot = " + pivot);
-        console.log("Index Pivot = " + indexRand);
-        console.log("After Sorting = " + left.concat(pivot, right));
-        console.log("");
+
+        this.showProcess(pivot, indexRand, left, right);
+
         return this.quickSort(left).concat(pivot, this.quickSort(right));
     }
     
-    processData(input) {
-        console.log("Samples = " + input);
-        console.log("");
-    
-        let arr = input.split(",");
+    processData() {    
+        let arr = this.data.split(",");
         
         for (let i = 0; i < arr.length; i++) {
             arr[i] = parseInt(arr[i]);
         }
-        console.log("Result = " + this.quickSort(arr).join())
+        
+        this.data = this.quickSort(arr).join();
+    }
+
+    showProcess(pivot, indexRand, left, right) {
+        console.log("Pivot = " + pivot);
+        console.log("Index Pivot = " + indexRand);
+        console.log("After Sorting = " + left.concat(pivot, right) + "\n");
+    }
+
+    do() {
+        console.log("Samples = " + this.data + "\n");
+
+        this.processData();
+
+        console.log("Result = " + this.data)
     }
 }
 
 
 // let sample = "-5,5,-4,4,-3,3,-2,2,-1,1,0";
-let sample = "-10,5,-8,4,-7,3,20,2,23,60,20,-1,1,18,0,11";
-let quicksort = new QuickSort();
+const sample = "-10,5,-8,4,-7,3,20,2,23,60,20,-1,1,18,0,11";
+const quicksort = new QuickSort(sample);
 
-quicksort.processData(sample);
+quicksort.do();
