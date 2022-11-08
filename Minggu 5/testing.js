7710115f91360cf55788960062b3f62f60543d1c
ArchieDylan <archie3036@gmail.com>
2022-11-08 12:49:10 +0800

    show tree

--- Minggu 5/testing.js
+++ Minggu 5/testing.js
@@ -43,51 +43,51 @@ class Tree {
         if (this.root == null) {
             this.root = node;
         } else {
-            this.insertNode(this.root, node);
+            this.insertHelper(this.root, node);
         }
     }
 
-    insertNode(node, newNode) {
+    insertHelper(node, newNode) {
         if (newNode.getValue() < node.getValue()) {
             if (!node.getLeft()) {
                 node.setLeft(newNode);
                 newNode.setParent(node);
             } else {
-                this.insertNode(node.left, newNode);
+                this.insertHelper(node.left, newNode);
             }
         } else {
             if (!node.getRight()) {
                 node.setRight(newNode);
                 newNode.setParent(node);
             } else {
-                this.insertNode(node.right,newNode);
+                this.insertHelper(node.right,newNode);
             }
         }   
     }
 
-    isExist(value) {
-        let node = this.root;
-        console.log(node)
-        if (!node) {
-            return false;
-        }
-
+    isExist(value, node = this.root) {
         if (node.getValue() == value) {
             return true;
         }
 
-        const test1 = this.isExist(value, node.getLeft());
-
-        if (test1) {
-            return true;
+        if (node.getLeft()) {
+            const left = this.isExist(value, node.getLeft());
+            if (left) {
+                return true;
+            }
+        } 
+        
+        if (node.getRight()) {
+            const right = this.isExist(value, node.getRight());
+            if (right) {
+                return true;
+            }
         }
 
-        const test2 = this.isExist(value, node.getRight());
-
-        return test2;
+        return false;
     }
 
-    showNode(node = this.root) {
+    inOrder(node = this.root) {
         if (node.getLeft()) {
             this.showNode(node.getLeft());
         } 
@@ -97,17 +97,80 @@ class Tree {
         }
         console.log(node.getValue());
     }
+
+    preOrder() {
+        return this.preOrderHelper(this.root);
+    }
+
+    preOrderHelper(node) {
+        if (node) {
+            console.log(node.getValue());
+            this.preOrderHelper(node.getLeft());
+            this.preOrderHelper(node.getRight());
+        }
+    }
+
+    height(root){
+        if (!root){
+            return 0
+        }
+        return Math.max(this.height(root.left), this.height(root.right))+1
+    }
+ 
+ 
+    getcol(h){
+        if (h == 1){
+            return 1
+        }
+        return this.getcol(h-1) + this.getcol(h-1) + 1
+    }
+
+    printTree(M, root, col, row, height){
+        if (!root){
+            return
+        }
+        // const substract (i) =>  
+        M[row][col] = root.getValue()
+        this.printTree(M, root.getLeft(), col-Math.pow(2, height-2), row+1, height-1)
+        this.printTree(M, root.getRight(), col+Math.pow(2, height-2), row+1, height-1)
+    }
+
+
+    TreePrinter(){
+        let data = ""
+        let h = this.height(this.root)
+        let col = this.getcol(h)
+        let M =  Array.from({length:h},_ => Array.from({length:col},_=> null))
+        
+        this.printTree(M, this.root, Math.floor(col/2, 1), 0, h)
+        // console.log(M)
+        M.map(i => {
+            i.map(j => {
+                if (j == null){
+                    data += "  "
+                }else{
+                    data += `${j} `
+                }
+            })
+            console.log(data);
+            data = '';
+        })
+    }
+
 }
 
 const data = new Tree();
 data.insert(5);
 data.insert(8);
 data.insert(3);
+data.insert(10);
+data.insert(15);
 data.insert(6);
 data.insert(9);
 data.insert(7);
 data.insert(4);
+console.log(data.TreePrinter());
 
-console.log(data.isExist(2,data.root));
+//console.log(data.isExist(650));
 
 //console.log(data);
